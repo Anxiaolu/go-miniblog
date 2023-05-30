5039c9dc02084905d862e0e31e6e66b45f264ce4
anlu.hu <anlu.hu@hand-china.com>
2023-05-31 00:13:32 +0800

    s02

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/Anxiaolu/miniblog/internal/miniblog"
+	_ "go.uber.org/automaxprocs"
 )
 
 // Go 程序的默认入口函数(主函数).
 func main() {
-	fmt.Println("Hello MiniBlog!")
+	command := miniblog.NewMiniBlogCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Anxiaolu/miniblog
 
 go 1.19
+
+require (
+	github.com/spf13/cobra v1.6.1
+	go.uber.org/automaxprocs v1.5.2
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+)
